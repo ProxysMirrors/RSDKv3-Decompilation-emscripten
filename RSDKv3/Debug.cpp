df69f5162c359abf36f088eed0e297a925714ab0
MegAmi <angrybirdconnor@gmail.com>
2025-12-25 10:40:18 -0600

    Fix console log not displaying if EngineDebugMode is disabled

--- RSDKv3/Debug.cpp
+++ RSDKv3/Debug.cpp
@@ -2,6 +2,48 @@
 
 int touchFlags = 0;
 
+void PrintLog(const char *msg, ...)
+{
+    if (engineDebugMode || Engine.consoleEnabled) {
+        char buffer[0x100];
+
+        // make the full string
+        va_list args;
+        va_start(args, msg);
+        vsprintf(buffer, msg, args);
+        printf("%s\n", buffer);
+        sprintf(buffer, "%s\n", buffer);
+
+        if (engineDebugMode) {
+            char pathBuffer[0x100];
+#if RETRO_PLATFORM == RETRO_OSX || RETRO_PLATFORM == RETRO_UWP
+            if (!usingCWD)
+#if RETRO_PLATFORM == RETRO_OSX
+            {
+                char logBuffer[0x100];
+                getResourcesPath(logBuffer, sizeof(logBuffer));
+                sprintf(pathBuffer, "%s/log.txt", logBuffer);
+            }
+#else
+                sprintf(pathBuffer, "%s/log.txt", getResourcesPath());
+#endif
+            else
+                sprintf(pathBuffer, "log.txt");
+#elif RETRO_PLATFORM == RETRO_ANDROID
+            sprintf(pathBuffer, "%s/log.txt", gamePath);
+            __android_log_print(ANDROID_LOG_INFO, "RSDKv3", "%s", buffer);
+#else
+            sprintf(pathBuffer, BASE_PATH "log.txt");
+#endif
+            FileIO *file = fOpen(pathBuffer, "a");
+            if (file) {
+                fWrite(&buffer, 1, StrLength(buffer), file);
+                fClose(file);
+            }
+        }
+    }
+}
+
 void InitDevMenu()
 {
 #if RETRO_USE_MOD_LOADER
